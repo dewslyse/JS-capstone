82c8fefe3b126cb1d60041ca63e8f41268e4c99b
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-04-12 16:36:11 +0300

    Fix linter error

--- dist/main.js
+++ dist/main.js
@@ -116,7 +116,7 @@ eval("\n\n/* istanbul ignore next  */\nfunction styleTagTransform(css, styleElem
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.scss */ \"./src/style.scss\");\n\r\n\n\n//# sourceURL=webpack://JS-capstone/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.scss */ \"./src/style.scss\");\n\n\n\n//# sourceURL=webpack://JS-capstone/./src/index.js?");
 
 /***/ })
 
