e018ed2a6e625b6f9c6eea6def85156315d01ca6
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-04-16 03:03:00 +0300

    Add comment functions

--- src/modules/actions.js
+++ src/modules/actions.js
@@ -69,57 +69,97 @@ const displayDetails = () => {
   const commentButtons = document.querySelectorAll('.comments-btn');
   commentButtons.forEach((button) => {
     button.addEventListener('click', () => {
+      const projectDetails = document.querySelector('.popup-detail');
+      const main = document.querySelector('.major');
+      projectDetails.classList.add('showpopup');
+      main.classList.add('blur');
       // console.log(button.id);
       const movie = truncated[button.id - 1];
-      const popupDetail = `<article class="show-card" id="popup">
-          <div class="img-box">
-            <img class="img" src="${movie.image.medium}">
+      const popupDetail = `<article class="detail-container" id="popup">
+        <button type="button" id="popup-close" data-close-button class="close-button-project" >&times;</button>
+        <div class=" firstdetails">
+          <div class="img-container">
+            <img class="image" src="${movie.image.medium}">
           </div>
-          <div class="show-detail">
-            <div class="show-detail-top">
-              <h2 class="show-name">${movie.name}</h2>                
-              <i data-id="${movie.id}" class="fas fa-heart"></i>                
-            </div>
-            <div class="show-detail-bottom">
-                <div class="rate"> 
-                  <p><i class="fas fa-star"></i><span class="rating">${movie.rating.average}</span></p>
-                </div>
-            </div>         
+          <div class="show-summary">
+            <h2 class="show-name">${movie.name}</h2>
+            <p>${movie.summary}</p>
+            <br>
+            <ul>
+              <li>Language: ${movie.language}</li>
+              <li>runtime: ${movie.averageRuntime} minutes</li>
+            </ul>
+            <ul>
+              <li>Rating: ${movie.rating.average}/10</li>
+              <li>premiered: ${movie.premiered}</li>
+            </ul>               
           </div>
+        </div>            
           <form class="form">
-            <h2 class="form-caption">Add a comment</h2> 
+            <h2 class="add-title">Add a comment</h2> 
             <input type="text" name="add-name" id="add-name" placeholder="Your name" required> 
             <input type="text" name="add-insight" id="add-comment" placeholder="Your insights" required>
-            <div class="btn-container"> 
-              <button class="btn add-btn" type="submit">Comment</button> 
+            <div class="add-container"> 
+              <button class="add-btn" type="submit">Comment</button>
             </div>
           </form> 
         </article>
       `;
-      document.body.insertAdjacentHTML('afterbegin', popupDetail);
-      const commentList = document.createElement('ul');
+      projectDetails.innerHTML = popupDetail;
+      const closePopup = projectDetails.querySelector('[data-close-button]');
+      closePopup.addEventListener('click', () => {
+        projectDetails.classList.remove('showpopup');
+        main.classList.remove('blur');
+      });
 
+      const commentList = document.createElement('ul');
       getMovieComments(button.id).then((response) => {
         // console.log(response);
-        response.forEach((comment) => {
+        if (response.error) {
           commentList.innerHTML += `
-              <li>${comment.comment}</li>
+              <li>Be the first to comment</li>
             `;
-        });
+        } else {
+          response.forEach((comment) => {
+            commentList.innerHTML += `
+              <li>${comment.creation_date} ${comment.username}:${comment.comment}</li>
+            `;
+          });
+        }
       },
       (error) => {
         commentList.innerHTML += `
               <li>Be the first to comment</li>
             `;
       });
-      // const popupElement = document.getElementById('popup');
-      // popupElement.appendChild(commentList);
+      const popupElement = document.getElementById('popup');
+      popupElement.appendChild(commentList);
       const commentForm = document.querySelector('.form');
       commentForm.addEventListener('submit', (e) => {
         e.preventDefault();
         const userName = document.getElementById('add-name').value;
         const userComment = document.getElementById('add-comment').value;
-        postComment(movie.id, userName, userComment);
+        postComment(movie.id, userName, userComment).then((response) => {
+          getMovieComments(button.id).then((response) => {
+            commentList.innerHTML = '';
+            if (response.error) {
+              commentList.innerHTML += `
+              <li>Be the first to comment</li>
+            `;
+            } else {
+              response.forEach((comment) => {
+                commentList.innerHTML += `
+              <li>${comment.creation_date} ${comment.username}: ${comment.comment}</li>
+            `;
+              });
+            }
+          },
+          (error) => {
+            commentList.innerHTML += `
+              <li>Be the first to comment</li>
+            `;
+          });
+        });
         commentForm.reset();
       });
     });
